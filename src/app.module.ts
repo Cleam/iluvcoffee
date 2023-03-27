f39b0425ade437a80691745ffe6065b6e76b061f
lizhigao <lizhigao@021.com>
2023-03-27 15:32:30 +0800

    feat: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,22 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './coffees/entities/coffee.entity';
 import { Flavor } from './coffees/entities/flavor.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    CoffeesModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       autoLoadEntities: true,
       synchronize: true, // 生产环境必须改成false，否则会导致数据丢失。
     }),
+    CoffeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
