7297442634cabf05a1f1b73a0686db4541e1d035
lizhigao <lizhigao@021.com>
2023-03-31 18:03:27 +0800

    feat: middleware中间件的应用

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,4 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestMiddleware,
-  NestModule,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
