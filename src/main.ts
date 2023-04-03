a3caca4174d0601c72d10741e81f7ec2001e94b5
lizhigao <lizhigao@021.com>
2023-04-03 18:46:12 +0800

    feat: add swagger for doc and openapi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 // import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,6 +29,16 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffees Application')
+    .setVersion('1.0')
+    // .addTag('coffee')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
